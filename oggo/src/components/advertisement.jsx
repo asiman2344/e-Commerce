da92993cb2e415ff7f2ead49e4639ace0a756d75
asiman2344 <asimanm024@gmail.com>
2025-02-09 20:28:39 +0400

    yenilendi

--- oggo/src/components/advertisement.jsx
+++ oggo/src/components/advertisement.jsx
@@ -1,8 +1,13 @@
 import React, { useRef, useState } from 'react';
+import { useSelector } from 'react-redux';
 
 function Advertisement() {
     const fileInputRef = useRef(null);
     const [selectedImage, setSelectedImage] = useState(null);
+    const [showHidden, setShowHidden] = useState(false);
+    const categories = useSelector(state => state.city.categories);
+    const cities = useSelector(state => state.city.city);
+
 
     const handleButtonClick = () => {
         fileInputRef.current.click();
@@ -19,6 +24,14 @@ function Advertisement() {
         }
     };
 
+    const selectMainCategory = (e) => {
+        if (e.target.value === 'Nəqliyyat') {
+            setShowHidden(true);
+        }else{
+            setShowHidden(false);
+        }
+    }
+
     return (
         <div className='advertisement bg-gray-200 p-[3rem] absolute top-[100px] w-[52%]'>
             <div className="advertisement-wrapper flex justify-between">
@@ -29,15 +42,88 @@ function Advertisement() {
                     <div className="advertisement-input-group">
                         <div className='mb-[30px]'>
                             <div className='mb-[10px]'><label className='text-[#10375c]' htmlFor="">Əsas Kateqoriya</label></div>
-                            <select className='border border-gray-200 w-full px-[12px] py-[7px] rounded-sm outline-none' name="" id="">
+                            <select className='border border-gray-200 w-full px-[12px] py-[7px] rounded-sm outline-none' name="" id="" onChange={selectMainCategory}>
                                 <option className='text-[#495057]' value="">Siyahıdan seçin</option>
+                                {
+                                    categories.map((item, index) => (
+                                        <option key={index}>{item.name}</option>
+                                    ))
+                                }
                             </select>
                         </div>
 
+                        <div className={showHidden ? '' : 'hidden'}>
+                            <div className='mb-[30px]'>
+                                <div className='mb-[10px]'><label className='text-[#10375c]' htmlFor="">Kateqoriya</label></div>
+                                <select className='border border-gray-200 w-full px-[12px] py-[7px] rounded-sm outline-none' name="" id="">
+                                    <option className='text-[#495057]' value="">Siyahıdan seçin</option>
+                                </select>
+                            </div>
+
+                            <div className='mb-[30px]'>
+                                <div className='mb-[10px]'><label className='text-[#10375c]' htmlFor="">Alt Kateqoriya</label></div>
+                                <select className='border border-gray-200 w-full px-[12px] py-[7px] rounded-sm outline-none' name="" id="">
+                                    <option className='text-[#495057]' value="">Siyahıdan seçin</option>
+                                </select>
+                            </div>
+                        </div>
+
+                        <div className='hidden'>
+                            <div className='mb-[30px]'>
+                                <div className='mb-[10px]'><label className='text-[#10375c]' htmlFor="">Rəng Seçin</label></div>
+                                <select className='border border-gray-200 w-full px-[12px] py-[7px] rounded-sm outline-none' name="" id="">
+                                    <option className='text-[#495057]' value="">Siyahıdan seçin</option>
+                                </select>
+                            </div>
+
+                            <div className='mb-[30px]'>
+                                <div className='mb-[10px]'><label className='text-[#10375c]' htmlFor="">Mühərrik Həcmi</label></div>
+                                <input type="number" className='border placeholder-black w-full px-[12px] py-[7px] rounded-sm outline-none' />
+                            </div>
+
+                            <div className='mb-[30px]'>
+                                <div className='mb-[10px]'><label className='text-[#10375c]' htmlFor="">Yanacaq Tipi</label></div>
+                                <select className='border border-gray-200 w-full px-[12px] py-[7px] rounded-sm outline-none' name="" id="">
+                                    <option className='text-[#495057]' value="">Siyahıdan seçin</option>
+                                </select>
+                            </div>
+
+                            <div className='mb-[30px]'>
+                                <div className='mb-[10px]'><label className='text-[#10375c]' htmlFor="">Sürətlər qutusu</label></div>
+                                <select className='border border-gray-200 w-full px-[12px] py-[7px] rounded-sm outline-none' name="" id="">
+                                    <option className='text-[#495057]' value="">Siyahıdan seçin</option>
+                                </select>
+                            </div>
+
+                            <div className='mb-[30px]'>
+                                <div className='mb-[10px]'><label className='text-[#10375c]' htmlFor="">Kuzov Növü</label></div>
+                                <select className='border border-gray-200 w-full px-[12px] py-[7px] rounded-sm outline-none' name="" id="">
+                                    <option className='text-[#495057]' value="">Siyahıdan seçin</option>
+                                </select>
+                            </div>
+
+                            <div className='mb-[30px]'>
+                                <div className='mb-[10px]'><label className='text-[#10375c]' htmlFor="">Avtomobilin İstehsal Tarixi</label></div>
+                                <input type="number" className='border placeholder-black w-full px-[12px] py-[7px] rounded-sm outline-none' />
+                            </div>
+
+                            <div className='mb-[30px]'>
+                                <div className='mb-[10px]'><label className='text-[#10375c]' htmlFor="">Kilometraj</label></div>
+                                <input type="number" className='border placeholder-black w-full px-[12px] py-[7px] rounded-sm outline-none' />
+                            </div>
+
+
+                        </div>
+
                         <div className='mb-[30px]'>
-                            <div className='mb-[10px]'><label className='text-[#10375c]' htmlFor="">Əsas Kateqoriya</label></div>
+                            <div className='mb-[10px]'><label className='text-[#10375c]' htmlFor="">Şəhər Seçin</label></div>
                             <select className='border border-gray-200 w-full px-[12px] py-[7px] rounded-sm outline-none' name="" id="">
                                 <option className='text-[#495057]' value="">Şəhər</option>
+                                {
+                                    cities.map((item, index) => (
+                                        <option key={index}>{item.name}</option>
+                                    ))
+                                }
                             </select>
                         </div>
 
